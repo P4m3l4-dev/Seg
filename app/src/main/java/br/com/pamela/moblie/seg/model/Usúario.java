8d43d14ac23a35c250c13e1c15c5cc2e89e74eef
P4m3l4-dev <pamela.a11@aluno.ifsc.edu,br>
2025-03-24 11:44:54 -0300

    classe usuario

---
+++ app/src/main/java/br/com/pamela/moblie/seg/model/Usúario.java
@@ -0,0 +1,70 @@
+package br.com.pamela.moblie.seg.model;
+
+public class Usúario {
+    //atributos
+    private String match_parent;
+    private String phone;
+    private String textEmailAddress;
+ //   private String numberPassword;
+
+    //CONSTRUTOR
+    //public Usuario(JSONObject jp)
+  //  {
+    //    try
+      //  {
+          //  this.setId(jp.getInt("id"));
+           // this.setNome(jp.getString("nome"));
+           // this.setSenha(jp.getString("senha"));
+          //  this.setEmail(jp.getString("email"));
+           // this.setAceiteTermos(jp.getBoolean("aceite"));
+            //this.setDataNascimento(jp.getString("nascimento"));
+           // this.setSexo(jp.getInt("sexo"));
+           // this.setPerfil(jp.getInt("perfil"));
+     //   }
+        //catch
+       // (JSONException e)
+    //    {
+       //     e.printStackTrace();
+     //   }
+
+        // metodos
+   // public String getMatch_parent(){
+  //      return this.match_parent;
+ //   }
+  //  public void setMatch_parent() throws Exception {
+    //    if(match_parent.length()>4 && !match_parent.contains("1123")) {
+     //       this.match_parent=match_parent;
+     //   } else{
+    //        throw new Exception("Nome inválido");
+    //    }
+ //   }
+
+ //   public void setMatch_parent(String match_parent) {
+   //     this.match_parent = match_parent;
+  //  }
+
+  //  public String getPhone() {
+     //   return phone;
+  //  }
+
+   // public void setPhone(String phone) {
+  //      this.phone = phone;
+ //   }
+
+   // public String getTextEmailAddress() {
+   //     return textEmailAddress;
+//    }
+
+  //  public void setTextEmailAddress(String textEmailAddress) {
+//        this.textEmailAddress = textEmailAddress;
+ //   }
+
+//    public String getNumberPassword() {
+   //     return numberPassword;
+ //   }
+
+  //  public void setNumberPassword(String numberPassword) {
+ //       this.numberPassword = numberPassword;
+ //   }
+
+//}
